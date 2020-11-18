2b281b50723b58e6fd0ed8dd2b04a85e84051b77
Lu-sure <66886680+Lu-sure@users.noreply.github.com>
2020-11-18 12:26:02 +0200

    List a tiny JS world by Lu-sure

--- index.js
+++ index.js
@@ -6,10 +6,77 @@
    */
 
 // ======== OBJECTS DEFINITIONS ========
-// Define your objects here
+const dog = {
+   species: 'dog',
+   name: 'Easy',
+   gender: 'female',
+   age: '2',
+   legs: 4,
+   hands: 0,
+   saying: {text: 'woof-woof!'},
+   friendly: 'everyone'
+ };
 
+ const cat = {
+   species: 'cat',
+   name: 'Roxy',
+   gender: 'female',
+   age: '1',
+   legs: 4,
+   hands: 0,
+   saying: {text: 'meow!!'},
+   friendly: 'Makar, Alex, Ivanka'
+ };
+
+ const man = {
+   species: 'human',
+   name: 'Kolia',
+   gender: 'male',
+   age: '26',
+   legs: 2,
+   hands: 2,
+   saying: {text: 'Let`s start at 9 a.m!'},
+   friendly: 'Love, Macha, Easy, Ariy'
+ };
+
+ const woman = {
+   species: 'human',
+   name: 'Love',
+   gender: 'female',
+   age: '26',
+   legs: 2,
+   hands: 2,
+   saying: {text: 'Fine, ok!'},
+   friendly: 'Easy, Kolia, Masha, Serg'
+ };
+
+ const catWoman = {
+   species: 'cat+human',
+   name: 'Tigr',
+   gender: 'female',
+   age: '21',
+   legs: 2,
+   hands: 2,
+   saying: cat.saying,
+   friendly: 'Easy'
+ };
+
+const inhabitants = [dog, cat, man, woman, catWoman];
+const getPrintText = function (obj) {
+   const values = Object.values(obj); 
+   const trueValues = values.map(function (value) {
+      if (typeof value === 'object') {
+         return getPrintText(value);
+      }
+      return value;
+   })
+   return trueValues.join('; ');
+}
 
 // ======== OUTPUT ========
+for (let i=0; i<inhabitants.length; i++) {
+   print(getPrintText(inhabitants[i]));
+};
 /* Use print(message) for output.
    Default tag for message is <pre>. Use print(message,'div') to change containing element tag.
 
@@ -27,5 +94,3 @@
    print('human; <strong>John</strong>; male; 2; 2; <em>Hello world!</em>; Rex, Tom, Jenny');
    print('human; <strong>John</strong>; male; 2; 2; <em>Hello world!</em>; Rex, Tom, Jenny', 'div');
    */
-
-
