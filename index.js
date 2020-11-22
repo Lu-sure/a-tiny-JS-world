69ad17bc3467a31ebdf0af459dd439e532d247a9
Lu-sure <66886680+Lu-sure@users.noreply.github.com>
2020-11-22 16:26:15 +0200

    Update printing

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ const dog = {
    legs: 4,
    hands: 0,
    saying: {text: 'meow!!'},
-   friendly: 'Makar, Alex, Ivanka'
+   friendly: 'Lila'
  };
 
  const man = {
@@ -36,7 +36,7 @@ const dog = {
    legs: 2,
    hands: 2,
    saying: {text: 'Let`s start at 9 a.m!'},
-   friendly: 'Love, Macha, Easy, Ariy'
+   friendly: 'Love, Easy'
  };
 
  const woman = {
@@ -47,50 +47,31 @@ const dog = {
    legs: 2,
    hands: 2,
    saying: {text: 'Fine, ok!'},
-   friendly: 'Easy, Kolia, Masha, Serg'
+   friendly: 'Easy, Kolia'
  };
 
  const catWoman = {
    species: 'cat+human',
-   name: 'Tigr',
+   name: 'Lila',
    gender: 'female',
    age: '21',
    legs: 2,
    hands: 2,
    saying: cat.saying,
-   friendly: 'Easy'
+   friendly: 'Roxy, Kolia'
  };
 
 const inhabitants = [dog, cat, man, woman, catWoman];
-const getPrintText = function (obj) {
-   const values = Object.values(obj); 
-   const trueValues = values.map(function (value) {
-      if (typeof value === 'object') {
-         return getPrintText(value);
-      }
-      return value;
-   })
-   return trueValues.join('; ');
-}
+
+const getPrintText = (obj) => Object.values(obj)
+  .reduce((acc, value) => {
+      const perfix = acc !== '' ? '; ' : '';
+      let text = value;
+      if (value != null && !Array.isArray(value) && typeof value === 'object') text = getPrintText(value);
+      return `${acc}${perfix}${text}`;
+   }, '');
 
 // ======== OUTPUT ========
 for (let i=0; i<inhabitants.length; i++) {
    print(getPrintText(inhabitants[i]));
 };
-/* Use print(message) for output.
-   Default tag for message is <pre>. Use print(message,'div') to change containing element tag.
-
-   Message can contain HTML markup. You may also tweak index.html and/or styles.css.
-   However, please, REFRAIN from improving visuals at least until your code is reviewed
-   so code reviewers might focus on a single file that is index.js.
-   */
-
-/* Print examples:
-   print('ABC');
-   print('<strong>ABC</strong>');
-   print('<strong>ABC</strong>', 'div');
-
-   print('human; John; male; 2; 2; Hello world!; Rex, Tom, Jenny');
-   print('human; <strong>John</strong>; male; 2; 2; <em>Hello world!</em>; Rex, Tom, Jenny');
-   print('human; <strong>John</strong>; male; 2; 2; <em>Hello world!</em>; Rex, Tom, Jenny', 'div');
-   */
