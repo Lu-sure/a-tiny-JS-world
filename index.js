bc847f4b99dd8df7f8044920a15dc523214e5586
Lu-sure <66886680+Lu-sure@users.noreply.github.com>
2020-12-04 13:55:38 +0200

    Init post-OOP commit

--- index.js
+++ index.js
@@ -5,73 +5,118 @@
    Web app: _put project's github pages URL here_
    */
 
-// ======== OBJECTS DEFINITIONS ========
-const dog = {
-   species: 'dog',
-   name: 'Easy',
-   gender: 'female',
-   age: '2',
-   legs: 4,
-   hands: 0,
-   saying: {text: 'woof-woof!'},
-   friendly: 'everyone'
- };
+// ======== OBJECTS DEFINITIONS ======== #2
+// const properties = ['species', 'name', 'lastName', 'gender', 'age', 'legs', 'paws', 'hands', 'saying', 'friendly'];
 
- const cat = {
-   species: 'cat',
-   name: 'Roxy',
-   gender: 'female',
-   age: '1',
-   legs: 4,
-   hands: 0,
-   saying: {text: 'meow!!'},
-   friendly: 'Lila'
- };
+class Inhabitant {
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
+    // this.friendly = friendly;
+  }
 
- const man = {
-   species: 'human',
-   name: 'Kolia',
-   gender: 'male',
-   age: '26',
-   legs: 2,
-   hands: 2,
-   saying: {text: 'Let`s start at 9 a.m!'},
-   friendly: 'Love, Easy'
- };
+  getPrintText() {
+    // return properties.map(prop => this[prop]).join('. ');
+    return `My name is ${this.name}. I\'m ${this.age} years old.`
+  }
 
- const woman = {
-   species: 'human',
-   name: 'Love',
-   gender: 'female',
-   age: '26',
-   legs: 2,
-   hands: 2,
-   saying: {text: 'Fine, ok!'},
-   friendly: 'Easy, Kolia'
- };
+}
 
- const catWoman = {
-   species: 'cat+human',
-   name: 'Lila',
-   gender: 'female',
-   age: '21',
-   legs: 2,
-   hands: 2,
-   saying: cat.saying,
-   friendly: 'Roxy, Kolia'
- };
+class Human extends Inhabitant {
+  constructor(name, lastName, age, saying) {
+    super(name, age);
+    this.name = `${name} ${lastName}`;
+    this.species = 'human';
+    this.saying = saying;
+  }
 
-const inhabitants = [dog, cat, man, woman, catWoman];
+  getPrintText() {
+    return `${this.species}: ${super.getPrintText()} \"${this.saying}\"`;
+  }
+}
 
-const getPrintText = (obj) => Object.values(obj)
-  .reduce((acc, value) => {
-      const perfix = acc !== '' ? '; ' : '';
-      let text = value;
-      if (value != null && !Array.isArray(value) && typeof value === 'object') text = getPrintText(value);
-      return `${acc}${perfix}${text}`;
-   }, '');
+class Woman extends Human {
+  constructor(name, lastName, age, saying) {
+    super(name, lastName, age, saying);
+    this.gender = 'female';
+  }
 
-// ======== OUTPUT ========
-for (let i=0; i<inhabitants.length; i++) {
-   print(getPrintText(inhabitants[i]));
-};
+  beBeautiful() {
+    return `\"I\'m loved by ${randomInhabitant})) \"`;
+  }
+}
+
+class Man extends Human {
+  constructor(name, lastName, age, saying) {
+    super(name, lastName, age, saying);
+    this.gender = 'male';
+  }
+
+  beGentleman() {
+    return `\"I like you very much. Just as you are.\"`;
+  }
+}
+
+class Pet extends Inhabitant {
+  constructor(name, age) {
+    super(name, age);
+  }
+
+  beTooMuchNice() {
+    return `.. sleeping softly on the knees`;
+  }
+}
+
+class Cat extends Pet {
+  constructor(name, gender, age) {
+    super(name, age);
+    this.saying = 'meow!';
+    this.species = 'cat';
+    this.gender = gender;
+  }
+
+  getPrintText() {
+    return `${this.species}: ${super.getPrintText()} \"${this.saying}\"`
+  }
+}
+
+class Dog extends Pet {
+  constructor(name, gender, age) {
+    super(name, age);
+    this.saying = 'woof-woof!';
+    this.species = 'dog';
+    this.gender = gender;
+  }
+
+  getPrintText() {
+    return `${this.species}: ${super.getPrintText()} \"${this.saying}\"`
+  }
+}
+
+function specialize_with(o, S, prop) { { o[prop] = S[prop] } }
+
+class CatWoman extends Woman {
+  constructor(name, lastName, age, saying) {
+    super(name, lastName, age, saying);
+    specialize_with(this, new Cat(), 'saying');
+  }
+
+}
+
+const johnny = new Man('Johnny', 'Depp', 57, 'Hate my ex!');
+const love = new Woman('Love', 'Beautiful', 26, 'Love everyone!');
+const roxy = new Cat('Roxolana', 'female', 1);
+const easy = new Dog('Easy', 'female', 2,);
+const catWoman = new CatWoman('Selina', 'Kyle', 21, 'I\'m cat!');
+const inhabitants = [roxy, easy, johnny, love, catWoman];
+let randomInhabitant = inhabitants[Math.floor(Math.random() * (inhabitants.length))].name;
+
+print(love.getPrintText());
+print(love.beBeautiful());
+print(johnny.getPrintText());
+print(johnny.beGentleman());
+print(roxy.getPrintText());
+print(roxy.beTooMuchNice());
+print(easy.getPrintText());
+print(easy.beTooMuchNice());
+print(catWoman.getPrintText());
